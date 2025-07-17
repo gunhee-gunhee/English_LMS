8bb03c8fb5ca9a32f06632710b8bdef722c6daa4
INTERLINE_11 <INTERLINE_11@host.docker.internal>
2025-07-17 16:48:44 +0900

    마이페이지 참가버튼 개선 포인트계산2

---
+++ lms/src/main/java/com/english/lms/repository/PointRepository.java
@@ -0,0 +1,17 @@
+package com.english.lms.repository;
+
+import com.english.lms.entity.PointEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface PointRepository extends JpaRepository<PointEntity, Integer> {
+
+    // 결석 취소 처리 때 absent_date와 student_num으로 row를 찾음
+    Optional<PointEntity> findByStudentNumAndAbsentDate(Integer studentNum, LocalDate absentDate);
+
+    // ★ 학생별, 타입별 포인트 row 목록 (예: 무료/보강포인트 등)
+    List<PointEntity> findByStudentNumAndType(Integer studentNum, String type);
+}
