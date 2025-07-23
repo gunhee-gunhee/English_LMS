07c4200bb8c56b17b817656685b7292bbd62a0ee
gunhee-gunhee <rjsrjs12300@gmail.com>
2025-07-23 13:52:20 +0900

    textService 수정

--- lms/src/main/java/com/english/lms/service/TextService.java
+++ lms/src/main/java/com/english/lms/service/TextService.java
@@ -1,31 +1,10 @@
 package com.english.lms.service;
 
 import java.util.List;
-
-import org.springframework.stereotype.Service;
-
 import com.english.lms.entity.TextEntity;
-import com.english.lms.repository.TextRepository;
 
-import lombok.RequiredArgsConstructor;
-
-@Service
-@RequiredArgsConstructor
-public class TextService {
-    private final TextRepository textRepository;
-
-    public List<TextEntity> findAll() {
-        return textRepository.findAll();
-    }
-
-    public TextEntity addText(String name) {
-        TextEntity entity = new TextEntity();
-        entity.setName(name);
-        return textRepository.save(entity);
-    }
-
-    public void deleteText(Integer textNum) {
-        textRepository.deleteById(textNum);
-    }
-} 
-    
+public interface TextService {
+    List<TextEntity> findAll();
+    TextEntity addText(String name);
+    void deleteText(Integer textNum);
+}
