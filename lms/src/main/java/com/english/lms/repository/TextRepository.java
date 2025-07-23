c96a333f37397a8fcc46d4d4cbb113b99f7b7135
gunhee-gunhee <rjsrjs12300@gmail.com>
2025-07-23 13:24:23 +0900

    교재 마스터

---
+++ lms/src/main/java/com/english/lms/repository/TextRepository.java
@@ -0,0 +1,8 @@
+package com.english.lms.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.english.lms.entity.TextEntity;
+
+public interface TextRepository extends JpaRepository<TextEntity, Integer> {
+}
